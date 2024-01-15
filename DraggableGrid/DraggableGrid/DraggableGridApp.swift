1b9421a241dfb6b92464cedf6c84e304505d52d1
lluuksz30 <lluuksz30@gmail.com>
2024-01-15 21:19:47 +0100

    Initial project

---
+++ DraggableGrid/DraggableGrid/DraggableGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableGridApp.swift
+//  DraggableGrid
+//
+//  Created by Łukasz Łuczak on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DraggableGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
