4d2df45f48746b0d70c10f8ce456251c5b5dd623
lluuksz30 <lluuksz30@gmail.com>
2024-05-17 17:24:54 +0200

    Move extension

--- DraggableGrid/DraggableGrid/Models/Bubble.swift
+++ DraggableGrid/DraggableGrid/Models/Bubble.swift
@@ -11,3 +11,21 @@ struct Bubble: Identifiable {
     let id = UUID().uuidString
     let name: String
 }
+
+extension Bubble {
+    static let all: [Bubble] = [
+        Bubble(name: "Bubble 1"),
+        Bubble(name: "Bubble 2"),
+        Bubble(name: "Bubble 3"),
+        Bubble(name: "Bubble 4"),
+        Bubble(name: "Bubble 5"),
+        Bubble(name: "Bubble 6"),
+        Bubble(name: "Bubble 7"),
+        Bubble(name: "Bubble 8"),
+        Bubble(name: "Bubble 9"),
+        Bubble(name: "Bubble 10"),
+        Bubble(name: "Bubble 11"),
+        Bubble(name: "Bubble 12"),
+        Bubble(name: "Bubble 13")
+    ]
+}
